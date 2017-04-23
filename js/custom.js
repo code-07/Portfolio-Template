1fe1bd31ace273a7aea3e1d1fb4087bdce78855d
Abu Taher <abutaherku10@gmail.com>
2017-04-23 11:37:15 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,24 @@ $('.counter').each(function() {
     }
   });  
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    fadeIn: true,
+    margin:10,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
